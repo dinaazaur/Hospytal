5f71d09e4d8e38346371aad2cdfb889ffe069cc5
nazario <veliknazarius@gmail.com>
2019-10-12 21:44:16 +0300

    finished header-down, and started doctor's card

--- js/main.js
+++ js/main.js
@@ -1,22 +1,55 @@
 // language change pop-up
-const dropDowns = document.querySelectorAll('.nav-changeLanguage');
-const dropDown = document.querySelector('.nav-changeLanguage');
-const item     = document.querySelector('.dropdown-menu');
+const item = document.querySelector('.dropdown-menu');
 
-function myFunction() {
-  item.classList.toggle("show");
-}
-dropDown.onclick = myFunction;
+$('.nav-changeLanguage').click(function () {
+	$('.dropdown-menu').toggleClass("show");
+})
 
-window.onclick = function(event) {
-  if (!event.target.matches('.nav-changeLanguage')) {
-    item.classList.remove('show');
-  }
+window.onclick = function (event) {
+	if (!event.target.matches('.nav-changeLanguage')) {
+		item.classList.remove('show');
+	}
 }
 
 
 // expand animation 
 $(".collabse-btn").click(function () {
-  $(".header-table").toggleClass("header-table_active").focus;
-  $(this).toggleClass("collapsed");
+	$(".header-table").toggleClass("header-table_active").focus;
+	$(".header").toggleClass("header-active").focus;
+	$('.header_logo').toggleClass('header_logo_fix');
+	$(this).toggleClass("collapsed");
 });
+
+// gallery 
+$('.gallery-btn').click(function() {
+	$('.gallery').toggleClass('gallery-active').focus;
+	$('.status-btn').toggleClass('status-btn_inactive').focus;
+	$('.about-btn').toggleClass('active-about').focus;
+});
+// Gallery show
+
+
+
+
+// Gallery main
+$(document).ready(function () {
+	$('.gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function (item) {
+				return item.el.attr('title');
+			}
+		}
+	});
+});
+
+
